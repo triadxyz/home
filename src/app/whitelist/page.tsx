fcd4ab8169b96ceeb56ba0a43bea35aa8d7ff3a4
natan alves <natanaelalvesnot@gmail.com>
2024-05-16 14:41:55 -0300

    adjusts

--- src/app/whitelist/page.tsx
+++ src/app/whitelist/page.tsx
@@ -19,24 +19,6 @@ const WhiteListPage: React.FC = () => {
     <div className="flex flex-col h-full w-screen">
       <div className="flex flex-col relative z-10 h-full px-4">
         <div className="h-[calc(100vh-70px)] w-full max-w-[1330px] mx-auto flex flex-col justify-center">
-          <div
-            className={`transition-opacity duration-500 w-full ${
-              currentQuestionIndex !== 0 ? "opacity-0 invisible hidden" : "opacity-100"
-            }`}
-          >
-            <div className="relative flex items-center justify-center mx-auto mt-14 lg:mt-0 flex-col z-10 px-0">
-              <div className="absolute max-[768px]:w-full h-[500px] -top-1/5 left-1/2 -translate-x-1/2">
-                <Image
-                  width={800}
-                  height={500}
-                  objectFit="cover"
-                  className="h-full"
-                  src={require("../../../public/img/blur.png")}
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
           <WhiteList
             formStarted={formStarted}
             initForm={() => setFormStarted(true)}

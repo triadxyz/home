292e83acdccde3eb1094d072afcc17cd3a7c100e
Arthur Henrique <artuh787@gmail.com>
2024-10-08 14:57:16 -0300

    adjust image

--- src/components/card-new.tsx
+++ src/components/card-new.tsx
@@ -12,8 +12,8 @@ type CardNewProps = {
 
 const CardNew: React.FC<CardNewProps> = ({ data }) => {
   return (
-    <div className="w-full max-w-[440px]">
-      <img className="w-full h-[210px] lg:h-[260px]" src={data.img} alt="" />
+    <div className="w-full lg:max-w-[440px]">
+      <img className="w-full h-[210px] sm:h-[320px] lg:h-[260px]  object-cover" src={data.img} alt="" />
       <div className="flex justify-between font-medium text-white lg:text-xl mt-5 mb-5">
        <div className="w-3/4"> {data.title}{" "}</div>
         <Link target="_blank" href={data.link} rel="noopener noreferrer">

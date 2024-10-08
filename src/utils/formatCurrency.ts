6bb577b31e5c5fa9d734eae0c0d4ca20c09492bb
Arthur Henrique <artuh787@gmail.com>
2024-10-08 11:34:54 -0300

    fix

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,6 +1,3 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable consistent-return */
-
 export const formatCurrency = (
   number: number | undefined | null = 0,
   noCurrency?: boolean
@@ -8,20 +5,20 @@ export const formatCurrency = (
   if (number === undefined || number === null) return;
 
   if (Number.isNaN(number)) {
-    return '$0.00';
+    return "$0.00";
   }
 
-  let replaceNumber = number.toString().replace(',', '');
+  let replaceNumber = number.toString().replace(",", "");
 
-  if (replaceNumber.indexOf(',') !== -1)
-    replaceNumber = replaceNumber.replace(',', '.');
+  if (replaceNumber.indexOf(",") !== -1)
+    replaceNumber = replaceNumber.replace(",", ".");
 
   const finalValue = parseFloat(replaceNumber);
 
   if (finalValue < 1 && finalValue !== 0) {
-    return finalValue.toLocaleString('en-us', {
-      style: 'currency',
-      currency: 'USD',
+    return finalValue.toLocaleString("en-us", {
+      style: "currency",
+      currency: "USD",
       minimumFractionDigits: 2,
       maximumFractionDigits: 6,
     });
@@ -30,14 +27,14 @@ export const formatCurrency = (
   const numNew = finalValue * 100;
   const formattedValue = Math.floor(numNew) / 100;
 
-  const styleCurrency = formattedValue.toLocaleString('en-us', {
-    style: 'currency',
-    currency: 'USD',
+  const styleCurrency = formattedValue.toLocaleString("en-us", {
+    style: "currency",
+    currency: "USD",
   });
 
   if (!noCurrency) return styleCurrency;
 
-  return styleCurrency.replace('$', '');
+  return styleCurrency.replace("$", "");
 };
 
 export const formatBigNumber = (number: bigint | number, decimals = 6) =>
@@ -48,23 +45,23 @@ export const formatNumberToShortScale = (
   decimals?: number,
   currency?: boolean
 ) => {
-  if (!num) return '0';
+  if (!num) return "0";
 
   const value = formatBigNumber(num, decimals);
 
   if (value >= 1.0e9) {
     const numFormatted = (value / 1.0e9).toFixed(2);
-    return `${numFormatted.replace(/\.00$/, '')}B`;
+    return `${numFormatted.replace(/\.00$/, "")}B`;
   }
 
   if (value >= 1.0e6) {
     const numFormatted = (value / 1.0e6).toFixed(2);
-    return `${numFormatted.replace(/\.00$/, '')}M`;
+    return `${numFormatted.replace(/\.00$/, "")}M`;
   }
 
   if (value >= 1.0e3) {
     const numFormatted = (value / 1.0e3).toFixed(2);
-    return `${numFormatted.replace(/\.00$/, '')}K`;
+    return `${numFormatted.replace(/\.00$/, "")}K`;
   }
 
   if (currency) {
@@ -75,7 +72,7 @@ export const formatNumberToShortScale = (
 };
 
 export const formatUsdc = (value: number) => {
-  return `${formatCurrency(value)?.replace('$', '')} USDC`;
+  return `${formatCurrency(value)?.replace("$", "")} USDC`;
 };
 
 export const formatValueOfBig = (value: number = 0) => {
@@ -86,12 +83,12 @@ export const formatTRD = (value: number, bigNumber: boolean = true) => {
   if (bigNumber) {
     return formatCurrency(formatValueOfBig(value || 0))
       ?.toString()
-      .replace('$', '')
-      .replace('.00', '');
+      .replace("$", "")
+      .replace(".00", "");
   }
 
   return formatCurrency(value || 0)
     ?.toString()
-    .replace('$', '')
-    .replace('.00', '');
+    .replace("$", "")
+    .replace(".00", "");
 };

33efcf442291545a59aa7265050789d61d3bd882
dannpl <dancardosobr@gmail.com>
2024-12-16 19:30:08 -0300

    add base

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const cspHeader = `
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
+    connect-src 'self' https://api.triadfi.co/;
     object-src 'none';
     base-uri 'self';
     form-action 'self';

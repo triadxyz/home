0b0ec8bf64fe39c18710484f8e3ee623f8e06447
Natanael Alves <Natanaelalvesnot@gmail.com>
2024-12-20 19:43:48 -0300

    change multipliers

--- src/Shared/YieldsSection/StakeSection/index.tsx
+++ src/Shared/YieldsSection/StakeSection/index.tsx
@@ -7,7 +7,7 @@ import StatsRow from "../StatsRow";
 export const CardsStake = () => {
   return (
     <div className="text-white w-full py-6 space-y-6">
-      <div className="flex items-center px-2 xl:px-0 justify-center flex-col md:flex-row w-full gap-5">
+      <div className="flex items-center px-2 xl:px-0 justify-start flex-col md:flex-row w-full gap-5">
         {stakeCards.map((card) => (
           <StakeCard
             key={card.id}
@@ -31,15 +31,6 @@ export const CardsStake = () => {
 
 
 const stakeCards = [
-    {
-      id: 1,
-      imgSrc: "/img/trident.webp",
-      altText: "Triad Icon",
-      title: "Stake Triad",
-      apr: "5.96%",
-      description: "Stake your NFT and watch your earnings grow based on its rarity. The rarer the NFT, the higher the returns!",
-      bgColor: "bg-fire-yellow",
-    },
     {
       id: 2,
       imgSrc: "/img/nft.webp",

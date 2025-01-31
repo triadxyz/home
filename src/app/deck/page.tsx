302ffadf83e3ecd0169bad4c4ca195e403ba3ce6
dannpl <dancardosobr@gmail.com>
2025-01-31 15:34:24 +0800

    add deck

--- src/app/deck/page.tsx
+++ src/app/deck/page.tsx
@@ -3,12 +3,13 @@ import React from "react";
 const Deck: React.FC = () => {
   return (
     <div style={{ width: "100%", minHeight: "100vh" }}>
-      <img
-        style={{ minHeight: "100vh" }}
-        src="/img/pitch.png"
-        height="100%"
-        width="100%"
-        alt="Deck"
+      <iframe
+        src="/path-to-your-pdf.pdf"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+        }}
       />
     </div>
   );
